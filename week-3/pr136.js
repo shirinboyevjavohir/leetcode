f75750ad2af525124f68d24f7654c7d0ccac1b2a
Javokhir Shirinboyev <shirinboyevjavokhir@gmail.com>
2024-05-18 18:15:00 +0500

    delete two line code

--- week-3/pr136.js
+++ week-3/pr136.js
@@ -11,8 +11,6 @@ const singleNumber = function (nums) {
   return first;
 };
 
-const nums = [1];
-console.log(singleNumber(nums));
 /* Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 You must implement a solution with a linear runtime complexity and use only constant extra space.
